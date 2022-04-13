46743eba5c7dac9ef29b5f74d505082bc4c7e792
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-13 21:53:58 +0200

    Brew formula update for ion version v0.1.0

--- Formula/ion.rb
+++ Formula/ion.rb
@@ -5,20 +5,20 @@
 class Ion < Formula
   desc "The ion formula"
   homepage "https://github.com/mas2020-golang/homebrew-ion"
-  version "0.1.0-rc.2"
+  version "0.1.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0-rc.2/ion_0.1.0-rc.2_Darwin-x86_64.tar.gz"
-      sha256 "47c13d474439749b82daba74adf353b1fb2d6e9772b211a734568a9534521020"
+    if Hardware::CPU.arm?
+      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0/ion_0.1.0_Darwin-arm64.tar.gz"
+      sha256 "4ceb044070767e3e62bcb588c9ceaa31558ae100221218bcf604b805315a7dd7"
 
       def install
         bin.install "ion"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0-rc.2/ion_0.1.0-rc.2_Darwin-arm64.tar.gz"
-      sha256 "bd9ab92f395a5b67c6f2b4a6b1993856f78e9895d6ac8537f4bea7d78cefff2e"
+    if Hardware::CPU.intel?
+      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0/ion_0.1.0_Darwin-x86_64.tar.gz"
+      sha256 "f31a0bb6c4e9688ab5ef293b5e5d8c8375dec69927ec9b84c325084515f9ccaf"
 
       def install
         bin.install "ion"
@@ -27,17 +27,17 @@ class Ion < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0-rc.2/ion_0.1.0-rc.2_Linux-x86_64.tar.gz"
-      sha256 "445d13d93c7a3f679ac3115d18c990509db3fef28f64368fa55b74901f3106ee"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0/ion_0.1.0_Linux-arm64.tar.gz"
+      sha256 "62f65bb03868d47c3bba50a85e1f424080e6e1f4a051575a9bf4ed3ca8ff4635"
 
       def install
         bin.install "ion"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0-rc.2/ion_0.1.0-rc.2_Linux-arm64.tar.gz"
-      sha256 "01e8dee661fc9307dc83b7852f5ef334f67f1fe536bfbe0a192479c21418e7bc"
+    if Hardware::CPU.intel?
+      url "https://github.com/mas2020-golang/ion/releases/download/v0.1.0/ion_0.1.0_Linux-x86_64.tar.gz"
+      sha256 "1901f54052dfa7ee86caad64a0e4257dd5dc57057c8f8e950af96417738eb292"
 
       def install
         bin.install "ion"
